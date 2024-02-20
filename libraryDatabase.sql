dbe0d9dc05a14bfe86662e23fa8e5584a24fb43a
venkateshbonala13 <venkateshbonala13@gmail.com>
2024-02-20 15:18:27 -0600

    library

---
+++ libraryDatabase.sql
@@ -0,0 +1,14 @@
+use library
+create table Member(
+memberID bigint,
+name varchar(30),
+email varchar(50),
+phone_number bigint
+)
+
+create table book(
+bookID varchar(30),
+title varchar(30),
+author varchar(30),
+price double
+)
